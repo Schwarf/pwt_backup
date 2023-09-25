6128e3a38e22015ac09a8de68ac7f475badf8fce
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-09-25 19:08:33 +0200

    WIP

---
+++ src/access.py
@@ -0,0 +1,20 @@
+import pyodbc
+
+
+def get_pyodbc_connection_string(password_file: str) -> str:
+    driver = "ODBC Driver 17 for SQL Server"
+    server = 'localhost'
+    database = 'pwt_backup'
+    with open(password_file, "r") as file:
+        lines = file.read().splitlines()
+        username = lines[0]
+        password = lines[1]
+        file.close()
+    url = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    return url
+
+
+def get_database_connection(password_file: str) -> pyodbc.Connection:
+    url = get_pyodbc_connection_string(password_file)
+    connection = pyodbc.connect(url)
+    return connection
