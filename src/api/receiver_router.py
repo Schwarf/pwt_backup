0487b0b4b95a67547ae6252d6f20ff7da7c8a58b
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-10-08 15:09:34 +0200

    Minor refactoring.

--- src/api/receiver_router.py
+++ src/api/receiver_router.py
@@ -14,7 +14,7 @@ async def insert_workout(workouts_received: WorkoutListReceived,
                          database: pyodbc.Connection = Depends(get_database_connection)):
     try:
         for workout in workouts_received.workouts:
-                insert(query.insert_workout, workout, database)
+            insert(query.insert_workout, workout, database)
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error in Workouts: {e}")
     return {"status": "Workout inserted successfully"}
@@ -32,22 +32,22 @@ async def insert_training(trainings_received: TrainingListReceived,
 
 
 @receiver_router.post("/insert_workout_timestamps/")
-async def insert_workout(workout_timestamps_received: WorkoutTimestampListReceived,
-                         database: pyodbc.Connection = Depends(get_database_connection)):
+async def insert_workout_timestamps(workout_timestamps_received: WorkoutTimestampListReceived,
+                                    database: pyodbc.Connection = Depends(get_database_connection)):
     try:
-        for workout_timestamp in workout_timestamps_received.workout_timestamp:
-                insert(query.insert_workout_timestamp, workout_timestamp, database)
+        for workout_timestamp in workout_timestamps_received.workout_timestamps:
+            insert(query.insert_workout_timestamp, workout_timestamp, database)
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error in Workout Timestamps: {e}")
     return {"status": "Workout Timestamp inserted successfully"}
 
 
 @receiver_router.post("/insert_training_timestamps/")
-async def insert_training(training_timestamps_received: TrainingTimestampReceived,
-                         database: pyodbc.Connection = Depends(get_database_connection)):
+async def insert_training_timestamps(training_timestamps_received: TrainingTimestampReceived,
+                                     database: pyodbc.Connection = Depends(get_database_connection)):
     try:
-        for training_timestamp in training_timestamps_received.training_timestamp:
-                insert(query.insert_training_timestamp, training_timestamp, database)
+        for training_timestamp in training_timestamps_received.training_timestamps:
+            insert(query.insert_training_timestamp, training_timestamp, database)
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error in Training Timestamps: {e}")
     return {"status": "Training Timestamp inserted successfully"}
@@ -68,7 +68,7 @@ async def update_workout(workouts_received: WorkoutListReceived,
 async def update_training(trainings_received: TrainingListReceived,
                           database: pyodbc.Connection = Depends(get_database_connection)):
     try:
-        for training in trainings_received.trainings    :
+        for training in trainings_received.trainings:
             update(query.update_training, training, database)
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error: {e}")
